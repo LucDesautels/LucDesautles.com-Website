de5a8581f03c146aa9f4ce9cc301c9f3adccc181
LucDesautels <152923222+LucDesautels@users.noreply.github.com>
2026-05-28 11:59:22 -0400

    Update site URL in astro.config.mjs

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   // Update this when you deploy. Used for canonical URLs.
-  site: "https://desautels.net",
+  site: "https://lucdesautels.com",
   integrations: [react()],
   vite: {
     build: {
